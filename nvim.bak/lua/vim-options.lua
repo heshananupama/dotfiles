6bae0c8b345d3dfe98bc05e4a1d55fb38e9b7337
Heshan Perera <heshanperera@mac.myfiosgateway.com>
2025-03-23 17:02:33 -0400

    Added my tmux configuration

--- nvim.bak/lua/vim-options.lua
+++
@@ -1,9 +0,0 @@
-vim.cmd("set clipboard+=unnamedplus")
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.keymap.set("n", "<C-z>", ':undo<CR>', {})
-vim.g.mapleader=" "
-
-
